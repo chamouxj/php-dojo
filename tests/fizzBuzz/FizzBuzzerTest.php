8673cdeb08465042c78488cbb4f889dbd0080a5a
vdebes <vdebes.externe@bedrockstreaming.com>
2020-12-24 18:58:27 +0100

    test class outputs 100 strings separated by new line FAIL

--- tests/fizzBuzz/FizzBuzzerTest.php
+++ tests/fizzBuzz/FizzBuzzerTest.php
@@ -23,10 +23,19 @@ class FizzBuzzerTest extends TestCase
         self::assertTrue(true);
     }
 
-    /** @testdox test class outputs strings */
+    /** @testdox test class outputs string */
     public function testOutputsString()
     {
         $testedInstance = new FizzBuzzer();
         self::assertIsString($testedInstance());
+    }
+
+    /** @testdox test class outputs 100 strings separated by a new line character */
+    public function testOutputs100Strings()
+    {
+        $testedInstance = new FizzBuzzer();
+        $output = $testedInstance();
+        self::assertIsString($output);
+        self::assertCount(100, explode(PHP_EOL, $output));
     }
 }
