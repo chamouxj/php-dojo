03a04de757ddb6b0bcf2a4ea34fdbf7840fdca23
vdebes <vdebes.externe@bedrockstreaming.com>
2020-12-24 18:35:47 +0100

    first test

---
+++ tests/fizzBuzz/FizzBuzzerTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace phpdojo\tests\fizzBuzz;
+
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzerTest extends TestCase
+{
+    public function testOutPutsStrings()
+    {
+        $testedInstance = new FizzBuzzer();
+    }
+}
