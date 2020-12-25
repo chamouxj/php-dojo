398ffe18f0927b6427da2931466dd298079a4dd4
vdebes <vdebes.externe@bedrockstreaming.com>
2020-12-25 23:19:09 +0100

    test is instanciable FAIL

---
+++ tests/unit/helloWorld/FramedGreeterTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace phpdojo\tests\unit\helloWorld;
+
+use PHPUnit\Framework\TestCase;
+
+class FramedGreeterTest extends TestCase
+{
+    /**
+     * @testdox Framed greeter surrounds the message with * characters
+     *
+     * This is a good example of how to guide a pair toward an implementation choice while working in ping-pong.
+     */
+    public function testFramedGreeter(): void
+    {
+        $testedInstance = new FramedGreeter();
+
+        self::assertIsObject($testedInstance);
+    }
+}
