b172848593fcd459794e125fcc632588aa5f4b28
vdebes <vdebes.externe@bedrockstreaming.com>
2020-12-25 21:39:11 +0100

    fix CS

--- tests/fizzBuzz/FizzBuzzerTest.php
+++ tests/fizzBuzz/FizzBuzzerTest.php
@@ -7,27 +7,34 @@ use vdebes\phpdojo\fizzBuzz\FizzBuzzer;
 
 class FizzBuzzerTest extends TestCase
 {
+
     /**
      * @var array|int[]
      */
     private array $testIndexesForThree = [3,6,9,12,18,21,24,27];
+
     /**
      * @var array|int[]
      */
     private array $testIndexesForFive = [5,10,20,25];
+
     /**
      * @var array|int[]
      */
     private array $testIndexesForThreeAndFive = [15, 30];
 
-    /** @testdox class is instanciable */
+    /**
+     * @testdox class is instanciable
+     */
     public function testIsInstanciable(): void
     {
         $testedInstance = new FizzBuzzer();
         self::assertIsObject($testedInstance);
     }
 
-    /** @testdox class is invokable */
+    /**
+     * @testdox class is invokable
+     */
     public function testIsInvokable(): void
     {
         $testedInstance = new FizzBuzzer();
@@ -36,28 +43,34 @@ class FizzBuzzerTest extends TestCase
         self::assertTrue(true);
     }
 
-    /** @testdox class outputs string */
+    /**
+     * @testdox class outputs string
+     */
     public function testOutputsString(): void
     {
         $testedInstance = new FizzBuzzer();
         self::assertIsString($testedInstance());
     }
 
-    /** @testdox class outputs 100 strings separated by a new line character */
+    /**
+     * @testdox class outputs 100 strings separated by a new line character
+     */
     public function testOutputs100Strings(): void
     {
         $testedInstance = new FizzBuzzer();
-        $output = $testedInstance();
+        $output         = $testedInstance();
         self::assertIsString($output);
         self::assertCount(100, explode(PHP_EOL, $output));
     }
 
-    /** @testdox class outputs numeric strings or Fizz instead of multiples of 3 */
+    /**
+     * @testdox class outputs numeric strings or Fizz instead of multiples of 3
+     */
     public function testOutputsNumericStringsOrFizz(): void
     {
         $testedInstance = new FizzBuzzer();
-        $output = $testedInstance();
-        $stringAsArray = explode(PHP_EOL, $output);
+        $output         = $testedInstance();
+        $stringAsArray  = explode(PHP_EOL, $output);
         foreach ($stringAsArray as $index => $value) {
             if (++$index > end($this->testIndexesForThree)) {
                 break;
@@ -74,12 +87,14 @@ class FizzBuzzerTest extends TestCase
         }
     }
 
-    /** @testdox class outputs numeric strings or Buzz instead of multiples of 5 */
+    /**
+     * @testdox class outputs numeric strings or Buzz instead of multiples of 5
+     */
     public function testOutputsNumericStringsOrBuzz(): void
     {
         $testedInstance = new FizzBuzzer();
-        $output = $testedInstance();
-        $stringAsArray = explode(PHP_EOL, $output);
+        $output         = $testedInstance();
+        $stringAsArray  = explode(PHP_EOL, $output);
         foreach ($stringAsArray as $index => $value) {
             if (++$index > end($this->testIndexesForFive)) {
                 break;
@@ -96,12 +111,14 @@ class FizzBuzzerTest extends TestCase
         }
     }
 
-    /** @testdox class outputs numeric strings or FizzBuzz instead of multiples of 3 and 5 */
+    /**
+     * @testdox class outputs numeric strings or FizzBuzz instead of multiples of 3 and 5
+     */
     public function testOutputsNumericStringsOrFizzBuzz(): void
     {
         $testedInstance = new FizzBuzzer();
-        $output = $testedInstance();
-        $stringAsArray = explode(PHP_EOL, $output);
+        $output         = $testedInstance();
+        $stringAsArray  = explode(PHP_EOL, $output);
         foreach ($stringAsArray as $index => $value) {
             if (++$index > end($this->testIndexesForThreeAndFive)) {
                 break;
