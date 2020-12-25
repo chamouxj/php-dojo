09b79fe50912ed6a3a5680e77e742f1d1c8946d5
vdebes <vdebes.externe@bedrockstreaming.com>
2020-12-25 22:42:41 +0100

    test is instantiable FAIL

--- tests/unit/helloWorld/GreeterTest.php
+++ tests/unit/helloWorld/GreeterTest.php
@@ -6,5 +6,9 @@ use PHPStan\Testing\TestCase;
 
 class GreeterTest extends TestCase
 {
-
+    /** @testdox class is instantiable */
+    public function testIsInstiable(): void
+    {
+        $testedInstance = new Greeter();
+    }
 }
